c6e81ccfca5f31c0ac81a8f506801917500face6
David Brown <davidbrown@davids-MBP.hitronhub.home>
2022-04-20 09:19:06 -0500

    completed assinment 4 part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,36 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+
+
+function addItem(item){
+    basket.push(item)
+ if(basket.indexOf(item) !== -1){
+        return true}
+}
+addItem("apples");
+addItem("plums");
+addItem("oranges");
+
+
+
+function listItems(basket){
+    for(let i = 0; i < basket.length; i++){
+        console.log(basket[i])
+    }
+}
+
+
+ 
+function empty(basket){
+ if(basket !== []){
+   return  basket = [];
+ }
+}
+
+
+
+
